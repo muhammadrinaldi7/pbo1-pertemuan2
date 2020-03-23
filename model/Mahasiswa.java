b28ec8e00101b3ca1850fe87a8c6ddd09cc3c415
sondang01 <zamsondang@gmail.com>
2020-03-24 01:51:59 +0800

    Buat Class Main dan Database

---
+++ model/Mahasiswa.java
@@ -0,0 +1,10 @@
+package model;
+public class Mahasiswa{
+public static void tampilanInfo(){
+System.out.println();
+System.out.println("== Data Mahasiswa ===================");
+System.out.println("NPM : 18631025");
+System.out.println("Nama : Muhammad Rinaldi");
+System.out.println("=====================================");
+}
+}
